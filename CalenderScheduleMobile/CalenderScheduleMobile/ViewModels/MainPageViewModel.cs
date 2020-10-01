c38aed0d13deb383a03454b5315b7b4fba9a4d4b
Ammon Zerkle <ammonzerkle@gmail.com>
2020-10-01 09:33:43 -0600

    Added Form for adding a schedule item. Have not tested with API yet.

--- CalenderScheduleMobile/CalenderScheduleMobile/ViewModels/MainPageViewModel.cs
+++ CalenderScheduleMobile/CalenderScheduleMobile/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Commands;
+﻿using CalenderSchedule.Shared;
+using CalenderScheduleMobile.Services;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
@@ -10,10 +12,59 @@ namespace CalenderScheduleMobile.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel(INavigationService navigationService)
+        private readonly ICalenderService calenderService;
+        private Schedule scheduleItem;
+        public MainPageViewModel(INavigationService navigationService, ICalenderService calenderService)
             : base(navigationService)
         {
             Title = "Main Page";
+            scheduleItem = new Schedule();
+            this.calenderService = calenderService ?? throw new ArgumentNullException(nameof(calenderService));
         }
+
+
+        string name;
+        public string Name
+        {
+            get => name;
+            set { SetProperty(ref name, value); }
+        }
+        DateTime modelDate;
+        public DateTime ModelDate
+        {
+            get => modelDate;
+            set 
+            { 
+                SetProperty(ref modelDate, value); 
+            }
+        }
+        TimeSpan time;
+        public TimeSpan Time
+        {
+            get => time;
+            set { SetProperty(ref time, value); }
+        }
+        string description;
+        public string Description
+        {
+            get => description;
+            set { SetProperty(ref description, value); }
+        }
+
+        private DelegateCommand addSchedule;
+        public DelegateCommand AddSchedule =>
+            addSchedule ?? (addSchedule = new DelegateCommand(async () =>
+            {
+
+                scheduleItem.Name = Name;
+                scheduleItem.Day = ModelDate;
+                scheduleItem.Time = ModelDate + Time;
+                scheduleItem.Description = Description;
+                //await calenderService.AddSchedule(scheduleItem);
+                Name = null;
+                ModelDate = DateTime.Now;
+                Description = null;
+            }));
+
     }
 }
